9095625e6853978ea501c56030a2e275dd7b6790
Alex Nguyen <dharmaturtle@gmail.com>
2019-03-16 17:51:54 -0500

    Added CardOverflow.Entity proj and Pluralizer

---
+++ CardOverflow.Entity/DesignTime/Pluralizer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace CardOverflow.Entity.DesignTime {
+  public class Pluralizer : IPluralizer {
+
+    public string Pluralize(string name) => 
+      Inflector.Inflector.Pluralize(name) ?? name;
+
+    public string Singularize(string name) => 
+      Inflector.Inflector.Singularize(name) ?? name;
+
+  }
+}
---
+++ CardOverflow.Entity/DesignTime/PluralizerDesignTimeService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardOverflow.Entity.DesignTime {
+  /// <summary>
+  /// If there is a class that implements IDesignTimeServices, 
+  /// then the EF Tools will call it to allow custom services 
+  /// to be registered.
+  /// </summary>
+  public class PluralizerDesignTimeService : IDesignTimeServices {
+
+    public void ConfigureDesignTimeServices(IServiceCollection services) {
+      services.AddSingleton<IPluralizer, Pluralizer>();
+    }
+
+  }
+}
