9fccf86e35f68ab3bea154af8a6a1ec1aea2db40
Alex Nguyen <dharmaturtle@gmail.com>
2019-03-21 23:09:30 -0500

    Converted merge to F# and tested it

---
+++ CardOverflow.Api/Core/Extensions.fs
@@ -0,0 +1,27 @@
+﻿namespace CardOverflow.Api
+
+open System.Collections.Generic
+open System.Linq
+
+module Extensions =
+  type IEnumerable<'TDest> with
+    member target.Merge<'TDest, 'TSource>
+      (source:IEnumerable<'TSource>) 
+      (predicate:'TDest * 'TSource -> bool)
+      (create:'TSource -> 'TDest)
+      (delete:'TDest -> unit)
+      (add:'TDest -> unit)
+      (update:'TDest -> 'TSource -> unit) =
+      let updates = [ for d in target do
+                      for s in source do
+                      yield (d,s)
+                    ] |> Seq.filter(predicate)
+      let adds = source |> Seq.filter(fun m -> not <| target.Any(fun s -> predicate(s,m))) |> Seq.toList
+      let deletes = target |> Seq.filter(fun s -> not <| source.Any(fun m -> predicate(s,m))) |> Seq.toList
+  
+      for d,s in updates do update d s
+      for d in deletes do delete d    
+      for item in adds do
+        let o = create item
+        update o item
+        add o
---
+++ CardOverflow.Test/MergeTests.fs
@@ -0,0 +1,47 @@
+module MergeTests
+
+open Xunit
+open CardOverflow.Api.Extensions
+
+type Person = { mutable id: int; mutable name: string }
+
+let merge source (target:ResizeArray<Person>) = 
+  target.Merge
+    (source)
+    (fun (x,y) -> x.id = y.id)
+    (id)
+    (target.Remove >> ignore)
+    (target.Add)
+    (fun d s -> d.name <- s.name)
+
+[<Fact>]
+let ``Merge with different source and target updates``() =
+  let newName0 = "new"
+  let newName1 = "newer"
+  let source = [{id=0; name=newName0}; {id=1; name=newName1}] |> ResizeArray<Person>
+  let target = [{id=0; name="old"}; {id=1; name="older"}] |> ResizeArray<Person>
+
+  merge source target
+
+  Assert.Equal(newName0, target.[0].name)
+  Assert.Equal(newName1, target.[1].name)
+
+[<Fact>]
+let ``Merge with more in source adds``() =
+  let source = [{id=0; name="x"}; {id=1; name="brand new"}] |> ResizeArray<Person>
+  let target = [{id=0; name="y"};] |> ResizeArray<Person>
+
+  merge source target
+
+  Assert.Equal("x", target.[0].name)
+  Assert.Equal("brand new", target.[1].name)
+
+[<Fact>]
+let ``Merge with more in target deletes``() =
+  let source = [{id=0; name="y"};] |> ResizeArray<Person>
+  let target = [{id=0; name="x"}; {id=1; name="I'm deleted"}] |> ResizeArray<Person>
+
+  merge source target
+
+  Assert.Equal("y", target.[0].name)
+  Assert.Single(target)
