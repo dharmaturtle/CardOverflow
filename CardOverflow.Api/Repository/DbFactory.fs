75ef5e9a1e70244f1dee047a218111a1806aff59
Alex Nguyen <dharmaturtle@gmail.com>
2019-04-07 19:55:55 -0500

    DbFactory uses 4 spaces

--- CardOverflow.Api/Repository/DbFactory.fs
+++ CardOverflow.Api/Repository/DbFactory.fs
@@ -4,22 +4,22 @@ open CardOverflow.Entity
 open Microsoft.EntityFrameworkCore
 
 type IConnectionStringProvider =
-  abstract Get: string
+    abstract Get: string
 
 type ConnectionStringProvider() =
-  interface IConnectionStringProvider with
-    member this.Get = "Server=localhost;Database=CardOverflow;Trusted_Connection=True;"
+    interface IConnectionStringProvider with
+        member this.Get = "Server=localhost;Database=CardOverflow;Trusted_Connection=True;"
 
 type DbFactory(connectionStringProvider: IConnectionStringProvider) =
-  member this.Create() =
-    DbContextOptionsBuilder().UseSqlServer(connectionStringProvider.Get).Options
-    |> fun o -> new CardOverflowDb(o)
+    member this.Create() =
+        DbContextOptionsBuilder().UseSqlServer(connectionStringProvider.Get).Options
+        |> fun o -> new CardOverflowDb(o)
 
 type DbService(dbFactory: DbFactory) =
-  member this.Query(q) =
-    use db = dbFactory.Create()
-    q db
-  member this.Command(q): unit =
-    use db = dbFactory.Create()
-    q db |> ignore
-    db.SaveChanges() |> ignore
+    member this.Query(q) =
+        use db = dbFactory.Create()
+        q db
+    member this.Command(q): unit =
+        use db = dbFactory.Create()
+        q db |> ignore
+        db.SaveChanges() |> ignore
