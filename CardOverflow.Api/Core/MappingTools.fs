4eb1fdecbfb82f434b33338a9039fbeb85c780cc
Alex Nguyen <dharmaturtle@gmail.com>
2019-04-28 11:14:14 -0500

    Created and tested Result.consolidate

--- CardOverflow.Api/Core/MappingTools.fs
+++ CardOverflow.Api/Core/MappingTools.fs
@@ -6,22 +6,22 @@ module MappingTools =
 
     let delimiter = ' '
 
-    let stringOfIntsToIntList(string: string) =
-        string.Split [|delimiter|] |> Seq.map int |> Seq.toList
+    let stringOfIntsToIntList (string: string) =
+        string.Split [| delimiter |] |> Seq.map int |> Seq.toList
 
-    let intsListToStringOfInts(ints: int list) =
+    let intsListToStringOfInts (ints: int list) =
         ints |> List.map string |> fun x -> String.Join(delimiter, x)
 
-    let stringOfMinutesToTimeSpanList(string: string) =
-        string.Split [|delimiter|] |> Seq.map (Double.Parse >> TimeSpan.FromMinutes) |> Seq.toList
+    let stringOfMinutesToTimeSpanList (string: string) =
+        string.Split [| delimiter |] |> Seq.map (Double.Parse >> TimeSpan.FromMinutes) |> Seq.toList
 
-    let timeSpanListToStringOfMinutes(timeSpans: TimeSpan list) =
+    let timeSpanListToStringOfMinutes (timeSpans: TimeSpan list) =
         timeSpans |> List.map (fun x -> x.TotalMinutes.ToString()) |> fun x -> String.Join(delimiter, x)
 
     let stringIntToBool =
         function
         | "0" -> false
-        | _-> true
+        | _ -> true
 
     let boolToString =
         function
@@ -31,7 +31,7 @@ module MappingTools =
     // https://en.wikipedia.org/wiki/C0_and_C1_control_codes
 
     let split separator (string: string) =
-        string.Split [|separator|] |> Array.toList
+        string.Split [| separator |] |> Array.toList
 
     let splitByFileSeparator =
         split '\x1c'
@@ -59,9 +59,9 @@ module MappingTools =
 
     let joinByUnitSeparator =
         join '\x1f'
-    
+
 module Option =
-    let fromNullable (n: _ Nullable) = 
+    let fromNullable (n: _ Nullable) =
         if n.HasValue
         then Some n.Value
         else None
@@ -74,3 +74,17 @@ module Result =
     let isOk = function
         | Ok _ -> true
         | Error _ -> false
+
+    let getOk = function
+        | Ok ok -> ok
+        | Error _ -> failwith "Not ok"
+
+    let getError = function
+        | Ok _ -> failwith "Not error"
+        | Error error -> error
+
+    let consolidate results =
+        let notOks = results |> List.filter (not << isOk)
+        if notOks.IsEmpty
+        then results |> List.map getOk |> Ok
+        else notOks |> List.map getError |> String.concat "\n" |> Error
---
+++ CardOverflow.Test/MappingToolsTests.fs
@@ -0,0 +1,55 @@
+module MappingToolsTests
+
+open CardOverflow.Api
+open Xunit
+
+[<Fact>]
+let ``Consolidating list of Ok ints is Ok``() =
+    [ 0; 1 ] |> List.map Ok
+    
+    |> Result.consolidate
+    
+    |> Result.isOk |> Assert.True
+
+[<Fact>]
+let ``Consolidating list of Ok ints yields ints``() =
+    let expected = [ 0; 1 ]
+    
+    let actual = expected |> List.map Ok |> Result.consolidate
+    
+    Assert.Equal<int>(expected, actual |> Result.getOk)
+
+[<Fact>]
+let ``Consolidating list of Errors is not Ok``() =
+    ["A"; "B"] |> List.map Error
+    
+    |> Result.consolidate |> Result.isOk
+    
+    |> Assert.False
+
+[<Fact>]
+let ``Consolidating list of Errors yields concatenated errors``() =
+    let actual =
+        ["A"; "B"]
+        |> List.map Error
+        |> Result.consolidate
+
+    Assert.Equal("A\nB", actual |> Result.getError)
+
+[<Fact>]
+let ``Consolidating list of int and Error is not Ok``() =
+    [ Ok 0; Error "" ]
+    
+    |> Result.consolidate |> Result.isOk
+    
+    |> Assert.False
+
+[<Fact>]
+let ``Consolidating list of int and Errors yields concatenated errors``() =
+    let actual =
+        [ Ok 0
+          Error "B"
+          Error "C" ]
+        |> Result.consolidate
+
+    Assert.Equal("B\nC", actual |> Result.getError)
