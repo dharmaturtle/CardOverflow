82aea959036601e367e77c2f3c500526fb550504
Alex Nguyen <dharmaturtle@gmail.com>
2019-06-06 19:47:50 -0500

    Added FAKE build

---
+++ build.fsx
@@ -0,0 +1,27 @@
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.create "Clean" (fun _ ->
+    !! "**/bin"
+    ++ "**/obj"
+    |> Shell.cleanDirs 
+)
+
+Target.create "Build" (fun _ ->
+    !! "**/*.*proj"
+    -- "**/*web*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "All" ignore
+
+"Clean"
+  ==> "Build"
+  ==> "All"
+
+Target.runOrDefault "All"
