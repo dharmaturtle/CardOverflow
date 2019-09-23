bb02726cc8cce6ba587ec33f9515795b73cc778d
A <dharmaturtle@gmail.com>
2019-09-23 18:38:53 -0500

    Tweaked Upload.razor

--- CardOverflow.Sanitation/Anki.fs
+++ CardOverflow.Sanitation/Anki.fs
@@ -8,6 +8,7 @@ open System
 open CardOverflow.Debug
 open CardOverflow.Pure
 open CardOverflow.Api
+open CardOverflow.Entity
 
 module SanitizeAnki = // medTODO actually sanitize, ie virus scan
     let ankiExportsDir = Directory.GetCurrentDirectory() +/ "AnkiExports"
@@ -25,5 +26,15 @@ module SanitizeAnki = // medTODO actually sanitize, ie virus scan
     let ankiDb pathToCollection =
         unzipCollectionToRandom pathToCollection
         |> AnkiDbFactory.Create
-    
+
+    let Import (db: CardOverflowDb) pathToCollection userId =
+        pathToCollection
+        |> AnkiImporter.loadFiles (fun sha256 -> db.File.FirstOrDefault(fun f -> f.Sha256 = sha256) |> Option.ofObj)
+        |> Result.bind (
+            AnkiImporter.save
+                db
+                (pathToCollection |> ankiDb |> AnkiImporter.getSimpleAnkiDb)
+                userId
+        )
+        
      // medTODO write a periodic function that will delete old files
