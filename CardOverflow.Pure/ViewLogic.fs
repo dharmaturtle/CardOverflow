92c712c843aef531bac240c1fcc3066ae65a8a47
A <dharmaturtle@gmail.com>
2019-10-17 21:02:24 -0500

    TimeSpan.toString works on negatives

--- CardOverflow.Pure/ViewLogic.fs
+++ CardOverflow.Pure/ViewLogic.fs
@@ -5,11 +5,11 @@ open Microsoft.FSharp.Core.Operators.Checked
 open System.ComponentModel.DataAnnotations
 open System.Text.RegularExpressions
 
-let toString =
-    function
-    | x when x < TimeSpan.FromMinutes 1. -> "1 min"
-    | x when x < TimeSpan.FromHours 1.   -> sprintf "%.0f min"   x.TotalMinutes
-    | x when x < TimeSpan.FromDays 1.    -> sprintf "%.0f h"     x.TotalHours
-    | x when x < TimeSpan.FromDays 30.   -> sprintf "%.0f d"     x.TotalDays
-    | x when x < TimeSpan.FromDays 365.  -> sprintf "%.1f mo" <| x.TotalDays / 30.
-    | x                                  -> sprintf "%.1f yr" <| x.TotalDays / 365.
+let toString (timeSpan: TimeSpan) =
+    match timeSpan.Duration() with
+    | duration when duration < TimeSpan.FromMinutes 1. -> if timeSpan >= TimeSpan.Zero then "1 min" else "-1 min"
+    | duration when duration < TimeSpan.FromHours 1.   -> sprintf "%.0f min"   timeSpan.TotalMinutes
+    | duration when duration < TimeSpan.FromDays 1.    -> sprintf "%.0f h"     timeSpan.TotalHours
+    | duration when duration < TimeSpan.FromDays 30.   -> sprintf "%.0f d"     timeSpan.TotalDays
+    | duration when duration < TimeSpan.FromDays 365.  -> sprintf "%.1f mo" <| timeSpan.TotalDays / 30.
+    | _                                                -> sprintf "%.1f yr" <| timeSpan.TotalDays / 365.
