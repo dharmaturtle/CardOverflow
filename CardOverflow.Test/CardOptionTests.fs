fa14ca8904ec80383e0b8f4c2df5d4e4c0d4c402
Alex <dharmaturtle@gmail.com>
2019-12-31 20:05:59 -0600

    defaultId => oldId

--- CardOverflow.Test/CardOptionTests.fs
+++ CardOverflow.Test/CardOptionTests.fs
@@ -23,18 +23,18 @@ let ``SanitizeCardOption.upsertMany can add/update new option``(): Task<unit> = 
     let userId = 3
     use c = new TestContainer()
     let! options = SanitizeCardOptionRepository.getAll c.Db userId
-    let defaultId = 3
-    Assert.Equal(defaultId, options.Single().Id)
+    let oldId = 3
+    Assert.Equal(oldId, options.Single().Id)
     let options =
         options.Append
             { ViewCardOption.load CardOptionsRepository.defaultCardOptions with
                 IsDefault = false }
         |> toResizeArray
-    let newId = defaultId + 1
+    let newId = oldId + 1
 
     let! id = SanitizeCardOptionRepository.upsertMany c.Db userId options
     
-    Assert.Equal(newId, (Result.getOk id).Single(fun x -> x  <> defaultId))
+    Assert.Equal(newId, (Result.getOk id).Single(fun x -> x  <> oldId))
     let! option = c.Db.CardOption.SingleAsync(fun x -> x.Id = newId)
     Assert.False option.IsDefault
 
@@ -44,24 +44,24 @@ let ``SanitizeCardOption.upsertMany can add/update new option``(): Task<unit> = 
     
     let! id =
         SanitizeCardOptionRepository.upsertMany c.Db userId
-            <| [options.Single(fun x -> x.Id = defaultId)
+            <| [options.Single(fun x -> x.Id = oldId)
                 { options.Single(fun x -> x.Id = newId) with Name = newName }
                ].ToList()
     
-    let id = (Result.getOk id).Single(fun x -> x  <> defaultId)
+    let id = (Result.getOk id).Single(fun x -> x  <> oldId)
     Assert.Equal(newId, id)
     Assert.Equal(newName, c.Db.CardOption.Single(fun x -> x.Id = id).Name)
 
-    let canUpdateIsDefault defaultIsDefault newIsDefault = task {
+    let canUpdateIsDefault oldIsDefault newIsDefault = task {
         let! options = SanitizeCardOptionRepository.getAll c.Db userId
     
         let! _ =
             SanitizeCardOptionRepository.upsertMany c.Db userId
-                <| [{ options.Single(fun x -> x.Id = defaultId) with IsDefault = defaultIsDefault }
-                    { options.Single(fun x -> x.Id = newId)     with IsDefault = newIsDefault }
+                <| [{ options.Single(fun x -> x.Id = oldId) with IsDefault = oldIsDefault }
+                    { options.Single(fun x -> x.Id = newId) with IsDefault = newIsDefault }
                    ].ToList()
     
-        Assert.Equal(defaultIsDefault, c.Db.CardOption.Single(fun x -> x.Id = defaultId).IsDefault)
+        Assert.Equal(oldIsDefault, c.Db.CardOption.Single(fun x -> x.Id = oldId).IsDefault)
         Assert.Equal(newIsDefault,     c.Db.CardOption.Single(fun x -> x.Id = newId).IsDefault) }
     do! canUpdateIsDefault false true
     do! canUpdateIsDefault true false
