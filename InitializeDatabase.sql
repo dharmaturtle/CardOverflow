40d925c106aa764846e848502d9daedf06f7923a
A <dharmaturtle@gmail.com>
2020-02-09 11:48:10 -0600

    Relationships are now on CardInstances (from Cards)

--- InitializeDatabase.sql
+++ InitializeDatabase.sql
@@ -1406,15 +1406,15 @@ ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[History] CHECK CONSTRAINT [FK_History_AcquiredCard_AcquiredCardId]
 GO
-ALTER TABLE [dbo].[Relationship]  WITH CHECK ADD  CONSTRAINT [FK_Relationship_Card_SourceId] FOREIGN KEY([SourceId])
-REFERENCES [dbo].[Card] ([Id])
+ALTER TABLE [dbo].[Relationship]  WITH CHECK ADD  CONSTRAINT [FK_Relationship_CardInstance_SourceId] FOREIGN KEY([SourceId])
+REFERENCES [dbo].[CardInstance] ([Id])
 GO
-ALTER TABLE [dbo].[Relationship] CHECK CONSTRAINT [FK_Relationship_Card_SourceId]
+ALTER TABLE [dbo].[Relationship] CHECK CONSTRAINT [FK_Relationship_CardInstance_SourceId]
 GO
-ALTER TABLE [dbo].[Relationship]  WITH CHECK ADD  CONSTRAINT [FK_Relationship_Card_TargetId] FOREIGN KEY([TargetId])
-REFERENCES [dbo].[Card] ([Id])
+ALTER TABLE [dbo].[Relationship]  WITH CHECK ADD  CONSTRAINT [FK_Relationship_CardInstance_TargetId] FOREIGN KEY([TargetId])
+REFERENCES [dbo].[CardInstance] ([Id])
 GO
-ALTER TABLE [dbo].[Relationship] CHECK CONSTRAINT [FK_Relationship_Card_TargetId]
+ALTER TABLE [dbo].[Relationship] CHECK CONSTRAINT [FK_Relationship_CardInstance_TargetId]
 GO
 ALTER TABLE [dbo].[Relationship]  WITH CHECK ADD  CONSTRAINT [FK_Relationship_User_UserId] FOREIGN KEY([UserId])
 REFERENCES [dbo].[User] ([Id])
