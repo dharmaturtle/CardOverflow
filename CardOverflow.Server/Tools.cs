9a4b5bf923428572d0039e228449a4b3c5f22c46
Alex <dharmaturtle@gmail.com>
2020-04-04 20:34:42 -0500

    Added GetQueryInt

---
+++ CardOverflow.Server/Tools.cs
@@ -0,0 +1,21 @@
+﻿using CardOverflow.Pure;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardOverflow.Server {
+  // Class members must be Pure! https://en.wikipedia.org/wiki/Pure_function
+  public static class Tools {
+    public static int GetQueryInt(this NavigationManager navigationManager, string key, int fallbackValue = 0) =>
+      navigationManager.Uri
+        .Apply(navigationManager.ToAbsoluteUri).Query
+        .Apply(QueryHelpers.ParseQuery)
+        .TryGetValue(key, out var token) &&
+        int.TryParse(token[0], out int possibleId)
+        ? possibleId
+        : fallbackValue;
+  }
+}
