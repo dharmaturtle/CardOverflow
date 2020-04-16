8d35944d5be75f36bdbb61b14db37d9130868c13
Alex <dharmaturtle@gmail.com>
2020-04-16 18:44:13 -0500

    Scaffolded ASP.NET Identity in IdentityProvider

---
+++ ThoughtDesign.IdentityProvider/Areas/Identity/Data/ThoughtDesignUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ThoughtDesign.IdentityProvider.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ThoughtDesignUser class
+    public class ThoughtDesignUser : IdentityUser
+    {
+    }
+}
---
+++ ThoughtDesign.IdentityProvider/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ThoughtDesign.IdentityProvider.Areas.Identity.Data;
+using ThoughtDesign.IdentityProvider.Data;
+
+[assembly: HostingStartup(typeof(ThoughtDesign.IdentityProvider.Areas.Identity.IdentityHostingStartup))]
+namespace ThoughtDesign.IdentityProvider.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDb>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityDbConnection")));
+
+                services.AddDefaultIdentity<ThoughtDesignUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityDb>();
+            });
+        }
+    }
+}
