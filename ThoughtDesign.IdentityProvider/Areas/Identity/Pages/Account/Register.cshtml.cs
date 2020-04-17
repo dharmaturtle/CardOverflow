00b4439a1862ebf488b3e4149b67e9393492305f
Alex <dharmaturtle@gmail.com>
2020-04-17 06:47:16 -0500

    Formatting

--- ThoughtDesign.IdentityProvider/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ThoughtDesign.IdentityProvider/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -15,101 +15,89 @@ using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using ThoughtDesign.IdentityProvider.Areas.Identity.Data;
 
-namespace ThoughtDesign.IdentityProvider.Areas.Identity.Pages.Account
-{
-    [AllowAnonymous]
-    public class RegisterModel : PageModel
-    {
-        private readonly SignInManager<ThoughtDesignUser> _signInManager;
-        private readonly UserManager<ThoughtDesignUser> _userManager;
-        private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+namespace ThoughtDesign.IdentityProvider.Areas.Identity.Pages.Account {
+  [AllowAnonymous]
+  public class RegisterModel : PageModel {
+    private readonly SignInManager<ThoughtDesignUser> _signInManager;
+    private readonly UserManager<ThoughtDesignUser> _userManager;
+    private readonly ILogger<RegisterModel> _logger;
+    private readonly IEmailSender _emailSender;
 
-        public RegisterModel(
-            UserManager<ThoughtDesignUser> userManager,
-            SignInManager<ThoughtDesignUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
-            _emailSender = emailSender;
-        }
-
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        public string ReturnUrl { get; set; }
-
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        public class InputModel
-        {
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
-        }
-
-        public async Task OnGetAsync(string returnUrl = null)
-        {
-            ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        }
-
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
-            {
-                var user = new ThoughtDesignUser { UserName = Input.Email, Email = Input.Email };
-                var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return Page();
-        }
+    public RegisterModel(
+        UserManager<ThoughtDesignUser> userManager,
+        SignInManager<ThoughtDesignUser> signInManager,
+        ILogger<RegisterModel> logger,
+        IEmailSender emailSender) {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _logger = logger;
+      _emailSender = emailSender;
     }
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public string ReturnUrl { get; set; }
+
+    public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+    public class InputModel {
+      [Required]
+      [EmailAddress]
+      [Display(Name = "Email")]
+      public string Email { get; set; }
+
+      [Required]
+      [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+      [DataType(DataType.Password)]
+      [Display(Name = "Password")]
+      public string Password { get; set; }
+
+      [DataType(DataType.Password)]
+      [Display(Name = "Confirm password")]
+      [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+      public string ConfirmPassword { get; set; }
+    }
+
+    public async Task OnGetAsync(string returnUrl = null) {
+      ReturnUrl = returnUrl;
+      ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+    }
+
+    public async Task<IActionResult> OnPostAsync(string returnUrl = null) {
+      returnUrl = returnUrl ?? Url.Content("~/");
+      ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+      if (ModelState.IsValid) {
+        var user = new ThoughtDesignUser { UserName = Input.Email, Email = Input.Email };
+        var result = await _userManager.CreateAsync(user, Input.Password);
+        if (result.Succeeded) {
+          _logger.LogInformation("User created a new account with password.");
+
+          var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+          code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+          var callbackUrl = Url.Page(
+              "/Account/ConfirmEmail",
+              pageHandler: null,
+              values: new { area = "Identity", userId = user.Id, code = code },
+              protocol: Request.Scheme);
+
+          await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+              $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+          if (_userManager.Options.SignIn.RequireConfirmedAccount) {
+            return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+          } else {
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return LocalRedirect(returnUrl);
+          }
+        }
+        foreach (var error in result.Errors) {
+          ModelState.AddModelError(string.Empty, error.Description);
+        }
+      }
+
+      // If we got this far, something failed, redisplay form
+      return Page();
+    }
+  }
 }
