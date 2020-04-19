ad1a17a4dc99ec7e8fbf108bdb8d05472cc42b22
Alex <dharmaturtle@gmail.com>
2020-04-19 11:59:51 -0500

    Added Index.cs

---
+++ CardOverflow.UserContentApi/Controllers/Index.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using CardOverflow.Api;
+using CardOverflow.Entity;
+using CardOverflow.Pure;
+using Microsoft.AspNetCore.Mvc;
+using ThoughtDesign.WebLibrary;
+
+namespace CardOverflow.UserContentApi.Controllers {
+  [ApiController]
+  //[Route("[controller]")]
+  public class Index : Controller {
+    private readonly UrlProvider _urlProvider;
+
+    public Index(UrlProvider urlProvider) =>
+      _urlProvider = urlProvider;
+
+    [HttpGet("/")]
+    public IActionResult Get() =>
+      Redirect(_urlProvider.ServerSideBlazor);
+
+  }
+}
