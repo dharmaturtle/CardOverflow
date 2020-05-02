837a3a5375ccc8394d8104e896bcb8a61c3ff4ed
Alex <dharmaturtle@gmail.com>
2020-05-02 15:03:13 -0500

    Added ``Tag counts work``

--- CardOverflow.Test/TagRepositoryTests.fs
+++ CardOverflow.Test/TagRepositoryTests.fs
@@ -1,6 +1,8 @@
 module TagRepositoryTests
 
 open CardOverflow.Entity
+open CardOverflow.Debug
+open CardOverflow.Pure
 open Microsoft.EntityFrameworkCore
 open CardOverflow.Api
 open CardOverflow.Test
@@ -66,4 +68,35 @@ let ``SanitizeTagRepository AddTo/DeleteFrom works``(): Task<unit> = (taskResult
     // Can't add a tag to a card that ain't yours
     let! error = SanitizeTagRepository.AddTo c.Db userId tagName cardId |> TaskResult.getError
     Assert.Equal(sprintf "User #%i doesn't have Card #%i." userId cardId, error)
+    } |> TaskResult.getOk)
+
+[<Fact>]
+let ``Tag counts work``(): Task<unit> = (taskResult {
+    use c = new TestContainer()
+    let assertTagUserCount expected =
+        c.Db.CardTagCount.SingleAsync() |> Task.map (fun x -> Assert.Equal(expected, x.Count))
+    let author = 1
+    let acquirer = 2
+    let cardId = 1
+    let cardInstanceId = 1001
+
+    // initial tag has 1 user
+    let tagName = Guid.NewGuid().ToString()
+    let! _ = FacetRepositoryTests.addBasicCard c.Db author [tagName]
+    do! assertTagUserCount 1
+
+    // initial tag has 2 users after acquisition
+    do! CardRepository.AcquireCardAsync c.Db acquirer cardInstanceId
+    do! SanitizeTagRepository.AddTo c.Db acquirer tagName cardId
+    do! assertTagUserCount 2
+
+    // suspending a card decrements the tag count
+    let! ac = CardRepository.GetAcquired c.Db acquirer cardId
+    do! CardRepository.editState c.Db acquirer ac.AcquiredCardId Suspended
+    do! assertTagUserCount 1
+
+    // deleting a card decrements the tag count
+    let! ac = CardRepository.GetAcquired c.Db author cardId
+    do! CardRepository.deleteAcquired c.Db author ac.AcquiredCardId
+    Assert.Empty <| c.Db.CardTagCount.ToList()
     } |> TaskResult.getOk)
