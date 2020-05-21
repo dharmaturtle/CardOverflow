685272a9329a8a81d85108a5bbb90743597f0b1c
Alex <dharmaturtle@gmail.com>
2020-05-21 17:37:30 -0500

    dotnet-watch-run uses WIndows Terminal

--- dotnet-watch-run.ps1
+++ dotnet-watch-run.ps1
@@ -1,4 +1,9 @@
-﻿Start-Process -NoNewWindow pwsh.exe -ArgumentList "-command", "dotnet watch --project CardOverflow.UserContentApi    run"
-Start-Process -NoNewWindow pwsh.exe -ArgumentList "-command", "dotnet watch --project CardOverflow.Server            run"
-Start-Process -NoNewWindow pwsh.exe -ArgumentList "-command", "dotnet watch --project ThoughtDesign.IdentityProvider run"
+$sp = "split-pane -p `"Command Prompt`""
+$d = Get-Location
+$_1 = "-d $d dotnet watch --project CardOverflow.Server            run;"
+$_2 = "-d $d dotnet watch --project ThoughtDesign.IdentityProvider run;"
+$_3 = "-d $d dotnet watch --project CardOverflow.UserContentApi    run"
+
+Start-Process -NoNewWindow wt -ArgumentList "$_1 $sp $_2 $sp $_3" # requires Windows Terminal
+
 Start "https://localhost:44315/"
