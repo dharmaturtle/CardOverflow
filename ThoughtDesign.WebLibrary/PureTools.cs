199069c250bcc9700cbb9d2abdadb640ac15c068
Alex <dharmaturtle@gmail.com>
2020-05-24 10:58:39 -0500

    StackRouter populates _branchId and _branchInstanceId

--- ThoughtDesign.WebLibrary/PureTools.cs
+++ ThoughtDesign.WebLibrary/PureTools.cs
@@ -11,6 +11,26 @@ using System.Threading.Tasks;
 namespace ThoughtDesign.WebLibrary {
   public static class PureTools {
 
+    public static bool TryGetQueryString<T>(this NavigationManager navManager, string key, out T value) { // https://chrissainty.com/working-with-query-strings-in-blazor/
+      var uri = navManager.ToAbsoluteUri(navManager.Uri);
+      if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString)) {
+        if ((typeof(T) == typeof(int) || typeof(T) == typeof(int?)) && int.TryParse(valueFromQueryString, out var valueAsInt)) {
+          value = (T)(object)valueAsInt;
+          return true;
+        }
+        if (typeof(T) == typeof(string)) {
+          value = (T)(object)valueFromQueryString.ToString();
+          return true;
+        }
+        if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal)) {
+          value = (T)(object)valueAsDecimal;
+          return true;
+        }
+      }
+      value = default;
+      return false;
+    }
+
     public static int GetQueryInt(this NavigationManager navigationManager, string key, int fallbackValue = 0) =>
       navigationManager.Uri
         .Apply(navigationManager.ToAbsoluteUri).Query
