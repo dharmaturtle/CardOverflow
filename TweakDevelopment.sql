f09cadb8c4957eedb7f09ad66953b97af8c5e510
Alex <dharmaturtle@gmail.com>
2020-05-30 09:05:20 -0500

    Added FK checks

--- TweakDevelopment.sql
+++ TweakDevelopment.sql
@@ -89,3 +89,26 @@ SELECT pg_catalog.setval('public."User_Id_seq"', 40, false);
 SELECT pg_catalog.setval('public."Filter_Id_seq"', 40, false);
 
 SET session_replication_role = 'origin';
+
+do $$ -- Verifies FKs (only) https://dba.stackexchange.com/a/250252
+  declare r record;
+BEGIN
+FOR r IN  (
+  SELECT FORMAT(
+    'UPDATE pg_constraint SET convalidated=false WHERE conname = TRIM(both ''"'' from ''%I''); ALTER TABLE %I VALIDATE CONSTRAINT %I;',
+    tc.constraint_name,
+    tc.table_name,
+    tc.constraint_name
+  ) AS x
+  FROM information_schema.table_constraints AS tc
+  JOIN information_schema.tables t ON t.table_name = tc.table_name and t.table_type = 'BASE TABLE'
+  JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
+  JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
+  WHERE  constraint_type = 'FOREIGN KEY'
+    AND tc.constraint_schema = 'public'
+)
+  LOOP
+    EXECUTE (r.x);
+  END LOOP;
+END;
+$$;
