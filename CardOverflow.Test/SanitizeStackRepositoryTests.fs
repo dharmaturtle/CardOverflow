3b8d9c8417960fd7cb4ba8b8c9b775fcf9f623e5
Alex <dharmaturtle@gmail.com>
2020-07-01 07:47:32 -0500

    // works on multiple acquired cards, e.g. reversedBasicCollate

--- CardOverflow.Test/SanitizeStackRepositoryTests.fs
+++ CardOverflow.Test/SanitizeStackRepositoryTests.fs
@@ -17,6 +17,7 @@ open System.Threading.Tasks
 open CardOverflow.Pure
 open CardOverflow.Sanitation
 open FsToolkit.ErrorHandling
+open FsCheck
 
 [<Fact>]
 let ``SanitizeStackRepository.Update with EditAcquiredCardCommands``(): Task<unit> = (taskResult {
@@ -29,28 +30,27 @@ let ``SanitizeStackRepository.Update with EditAcquiredCardCommands``(): Task<uni
                 { ViewCardSetting.load CardSettingsRepository.defaultCardSettings with
                     IsDefault = false })
         |>% toResizeArray
-    let! optionId =
+    let! optionIdGen =
         SanitizeCardSettingRepository.upsertMany c.Db userId options
-        |>%% Gen.sample1
-    let! deckId =
+        |>%% Gen.elements
+    let! deckIdGen =
         Guid.NewGuid().ToString()
         |> SanitizeDeckRepository.create c.Db userId
         |>%% fun newDeckId -> [ userId; newDeckId ]
-        |>%% Gen.sample1
-    let front = Guid.NewGuid().ToString()
-    let back = Guid.NewGuid().ToString()
-    let state = Gen.gen<CardState> |> Gen.sample1Gen
+        |>%% Gen.elements
+    let genCommand () =
+        {   CardSettingId = optionIdGen |> Gen.sample1Gen
+            DeckId = deckIdGen |> Gen.sample1Gen
+            CardState = Gen.gen<CardState> |> Gen.sample1Gen
+            FrontPersonalField = Guid.NewGuid().ToString()
+            BackPersonalField = Guid.NewGuid().ToString() }
     let! collate = FacetRepositoryTests.basicCollate c.Db
+    let basicCommand = genCommand ()
     let stackId = 1
     let branchId = 1
 
     do! SanitizeStackRepository.Update c.Db userId
-            [   {   CardSettingId = optionId
-                    DeckId = deckId
-                    CardState = state
-                    FrontPersonalField = front
-                    BackPersonalField = back
-            }   ]
+            [ basicCommand ]
             {   EditSummary = Guid.NewGuid().ToString()
                 FieldValues = [].ToList()
                 CollateInstance = collate
@@ -69,14 +69,67 @@ let ``SanitizeStackRepository.Update with EditAcquiredCardCommands``(): Task<uni
             BranchId = branchId
             BranchInstanceMeta = ac.BranchInstanceMeta // untested
             Index = 0s
-            CardState = state
+            CardState = basicCommand.CardState
             IsLapsed = false
             EaseFactorInPermille = 0s
             IntervalOrStepsIndex = NewStepsIndex 0uy
             Due = ac.Due // untested
-            CardSettingId = optionId
+            CardSettingId = basicCommand.CardSettingId
             Tags = []
-            DeckId = deckId
+            DeckId = basicCommand.DeckId
         }
         ac
+    
+    // works on multiple acquired cards, e.g. reversedBasicCollate
+    let! collate = FacetRepositoryTests.reversedBasicCollate c.Db
+    let stackId = 2
+    let branchId = 2
+    let a = genCommand ()
+    let b = genCommand ()
+
+    do! SanitizeStackRepository.Update c.Db userId
+            [ a; b ]
+            {   EditSummary = Guid.NewGuid().ToString()
+                FieldValues = [].ToList()
+                CollateInstance = collate
+                Kind = NewOriginal_TagIds []
+                Title = null
+            }
+        |>%% Assert.equal branchId
+
+    let! (acs: AcquiredCard ResizeArray) = StackRepository.GetAcquired c.Db userId stackId
+    Assert.equal
+        {   AcquiredCardId = 2
+            UserId = userId
+            StackId = stackId
+            BranchId = branchId
+            BranchInstanceMeta = acs.[0].BranchInstanceMeta // untested
+            Index = 0s
+            CardState = a.CardState
+            IsLapsed = false
+            EaseFactorInPermille = 0s
+            IntervalOrStepsIndex = NewStepsIndex 0uy
+            Due = acs.[0].Due // untested
+            CardSettingId = a.CardSettingId
+            Tags = []
+            DeckId = a.DeckId
+        }
+        acs.[0]
+    Assert.equal
+        {   AcquiredCardId = 3
+            UserId = userId
+            StackId = stackId
+            BranchId = branchId
+            BranchInstanceMeta = acs.[1].BranchInstanceMeta // untested
+            Index = 1s
+            CardState = b.CardState
+            IsLapsed = false
+            EaseFactorInPermille = 0s
+            IntervalOrStepsIndex = NewStepsIndex 0uy
+            Due = acs.[1].Due // untested
+            CardSettingId = b.CardSettingId
+            Tags = []
+            DeckId = b.DeckId
+        }
+        acs.[1]
     } |> TaskResult.getOk)
