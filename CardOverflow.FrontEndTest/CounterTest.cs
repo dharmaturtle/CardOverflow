4a9d07ae6ea39e893f59a7644dcf6d334eb95319
Alex <dharmaturtle@gmail.com>
2020-07-10 18:49:15 -0500

    Added bUnit and CounterTest

---
+++ CardOverflow.FrontEndTest/CounterTest.cs
@@ -0,0 +1,27 @@
+﻿using CardOverflow.Debug;
+using CardOverflow.Pure;
+using CardOverflow.Server.Pages;
+using CardOverflow.Server.Pages.Deck;
+using CardOverflow.Test;
+using FsCheck;
+using FsCheck.Xunit;
+using System;
+using System.Linq;
+using Xunit;
+using Bunit;
+using static CardOverflow.FrontEndTest.TestHelper;
+
+namespace CardOverflow.FrontEndTest {
+  public class CounterTest : TestContext {
+
+    [Fact]
+    public void ButtonClicked_Increments() {
+      var counter = RenderComponent<Counter>();
+
+      counter.Find("button").Click();
+
+      counter.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    }
+
+  }
+}
