ace99567f0b569c19e35e84329a87b04a9cbd354
Alex <dharmaturtle@gmail.com>
2020-07-11 16:45:41 -0500

    ``Submitting default FollowDeckCommand _ displays validation error`` is a property based test

--- CardOverflow.FrontEndTest/FollowButtonTests.cs
+++ CardOverflow.FrontEndTest/FollowButtonTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components;
 using CardOverflow.Server;
 using ThoughtDesign.WebLibrary;
@@ -18,32 +18,40 @@ using static Bunit.ComponentParameterFactory;
 using System;
 using CardOverflow.Server.Pages.Deck;
 using CardOverflow.Pure;
+using FsCheck;
+using FsCheck.Xunit;
 
 namespace CardOverflow.FrontEndTest {
   public class FollowButtonTests : TestContext {
-    [Fact(DisplayName = "Submitting default FollowDeckCommand _ displays validation error")]
-    public async Task _1() {
-      const int userId = 1;
-      const int defaultDeckId = 123;
-      await Setup(Services, db => new UserEntity {
-        Id = userId,
-        DefaultDeckId = defaultDeckId,
-        Decks = new List<DeckEntity> {
-          new DeckEntity {
-            Id = defaultDeckId,
-            Name = Guid.NewGuid().ToString(),
-          }
-        }
+
+    private static class FollowButtonTestsArb {
+      public static Arbitrary<UserClaims> UserClaims() =>
+        (from name in Arb.Generate<string>()
+         from email in Arb.Generate<string>()
+         from id in Arb.Generate<int>().Where(x => x != 0)
+         select new UserClaims(id, name, email)
+        ).Pipe(Arb.From);
+      public static Arbitrary<DeckEntity> DeckEntity() =>
+        Arb.Generate<int>()
+        .Select(id => new DeckEntity { Id = id })
+        .Pipe(Arb.From);
+    }
+
+    [Property(DisplayName = "Submitting default FollowDeckCommand _ displays validation error", Arbitrary = new[] { typeof(FollowButtonTestsArb) })]
+    public bool _1(UserClaims userClaims, PublicDeck publicDeck, DeckEntity usersDefaultDeck) {
+      Setup(Services, db => new UserEntity {
+        Id = userClaims.Id,
+        DefaultDeckId = usersDefaultDeck.Id,
+        Decks = new List<DeckEntity> { usersDefaultDeck }
       }.Pipe(db.User.Add));
-      var claims = new UserClaims(userId, Guid.NewGuid().ToString(), Guid.NewGuid().ToString()).Pipe(Task.FromResult);
       var counter = RenderComponent<FollowButtons>(
-        (nameof(FollowButtons.Deck), new PublicDeck()),
-        CascadingValue(claims)
+        (nameof(FollowButtons.Deck), publicDeck),
+        CascadingValue(Task.FromResult(userClaims))
       );
 
       counter.Find("form").Submit();
 
-      Assert.Equal("'New Deck Name' must not be empty.", counter.Find(".validation-message").TextContent);
+      return "'New Deck Name' must not be empty." == counter.Find(".validation-message").TextContent;
     }
 
   }
--- CardOverflow.FrontEndTest/TestHelper.cs
+++ CardOverflow.FrontEndTest/TestHelper.cs
@@ -1,6 +1,7 @@
-﻿using Blazored.Toast;
+using Blazored.Toast;
 using BlazorStrap;
 using Bunit;
+using CardOverflow.Debug;
 using CardOverflow.Entity;
 using CardOverflow.Server;
 using CardOverflow.Server.Pages.Deck;
@@ -8,6 +9,7 @@ using FluentValidation;
 using FsToolkit.ErrorHandling;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
@@ -22,18 +24,25 @@ namespace CardOverflow.FrontEndTest {
 
     public static bool Not(bool input) => !input;
 
-    public static Task<int> Setup(TestServiceProvider services, Action<CardOverflowDb> setupDb) {
-      services.AddSingleton<IEntityHasher, ContainerExtensions.EntityHasher>();
-      services.AddBlazoredToast();
-      services.AddScoped<NavigationManager, MockNavigationManager>();
-      services.AddBootstrapCss();
-      services.AddTransient<IValidator<FollowCommandViewModel>, FollowCommandViewModelValidator>();
-      services.AddSingleton<DbExecutor>();
-      services.AddDbContextPool<CardOverflowDb>(x => x.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+    public static void Setup(TestServiceProvider services, Action<CardOverflowDb> setupDb) {
+      if (!services.IsProviderInitialized) {
+        services.AddSingleton<IEntityHasher, ContainerExtensions.EntityHasher>();
+        services.AddBlazoredToast();
+        services.AddScoped<NavigationManager, MockNavigationManager>();
+        services.AddBootstrapCss();
+        services.AddTransient<IValidator<FollowCommandViewModel>, FollowCommandViewModelValidator>();
+        services.AddSingleton<DbExecutor>();
+        services.AddDbContextPool<CardOverflowDb>(x => x.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+      }
 
-      var databaseContext = services.GetService<CardOverflowDb>();
+      using var databaseContext = services.GetService<CardOverflowDb>();
+      databaseContext.Database.EnsureDeleted();
+      databaseContext.Database.EnsureCreated();
       setupDb(databaseContext);
-      return databaseContext.SaveChangesAsync();
+      databaseContext.SaveChanges();
+      foreach (var entity in databaseContext.ChangeTracker.Entries()) {
+        entity.State = EntityState.Detached;
+      }
     }
 
   }
