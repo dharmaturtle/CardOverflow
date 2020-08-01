32712272e02ba83891b5c0019e13d7af7a504b73
Alex <dharmaturtle@gmail.com>
2020-08-01 14:29:28 -0500

    to file0branch_instance

--- CardOverflow.Entity/File_BranchInstanceEntity.cs
+++ CardOverflow.Entity/File_BranchInstanceEntity.cs
@@ -1,10 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CardOverflow.Entity
 {
+    [Table("file0branch_instance")]
     public partial class File_BranchInstanceEntity
     {
         [Key]
