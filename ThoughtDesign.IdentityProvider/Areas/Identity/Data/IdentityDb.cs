1663c9d161ded91951772291b6ee9e211acf30b0
Alex <dharmaturtle@gmail.com>
2020-09-06 09:23:07 -0500

    Formatting

--- ThoughtDesign.IdentityProvider/Areas/Identity/Data/IdentityDb.cs
+++ ThoughtDesign.IdentityProvider/Areas/Identity/Data/IdentityDb.cs
@@ -7,20 +7,17 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ThoughtDesign.IdentityProvider.Areas.Identity.Data;
 
-namespace ThoughtDesign.IdentityProvider.Data
-{
-    public class IdentityDb : IdentityDbContext<ThoughtDesignUser, IdentityRole<Guid>, Guid> {
-        public IdentityDb(DbContextOptions<IdentityDb> options)
-            : base(options)
-        {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-        }
+namespace ThoughtDesign.IdentityProvider.Data {
+  public class IdentityDb : IdentityDbContext<ThoughtDesignUser, IdentityRole<Guid>, Guid> {
+    public IdentityDb(DbContextOptions<IdentityDb> options)
+        : base(options) {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+      base.OnModelCreating(builder);
+      // Customize the ASP.NET Identity model and override the defaults if needed.
+      // For example, you can rename the ASP.NET Identity table names and more.
+      // Add your customizations after calling base.OnModelCreating(builder);
+    }
+  }
 }
