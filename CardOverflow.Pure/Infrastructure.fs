2c69fa7db4612c16d9fda8c1ac8f4e1246a1cfb4
Alex <dharmaturtle@gmail.com>
2020-12-31 12:47:11 -0600

    Added the User stream

--- CardOverflow.Pure/Infrastructure.fs
+++ CardOverflow.Pure/Infrastructure.fs
@@ -7,6 +7,11 @@ open System
 type UserId = Guid<userId>
     and [<Measure>] userId
 
+type DeckId = Guid<deckId>
+    and [<Measure>] deckId
+type CardSettingId = Guid<cardSettingId>
+    and [<Measure>] cardSettingId
+
 type StackId = Guid<stackId>
     and [<Measure>] stackId
 type BranchId = Guid<branchId>
---
+++ CardOverflow.Pure/User.fs
@@ -0,0 +1,76 @@
+module Domain.User
+
+open Equinox
+open FsCodec
+open FsCodec.NewtonsoftJson
+open Serilog
+open TypeShape
+open NodaTime
+open CardOverflow.Pure
+
+let streamName (id: UserId) = StreamName.create "User" (id.ToString())
+
+// NOTE - these types and the union case names reflect the actual storage formats and hence need to be versioned with care
+[<RequireQualifiedAccess>]
+module Events =
+    
+    type Snapshotted =
+        {  UserId: UserId
+           DisplayName: string
+           DefaultCardSettingId: CardSettingId
+           DefaultDeckId: DeckId
+           ShowNextReviewTime: bool
+           ShowRemainingCardCount: bool
+           StudyOrder: StudyOrder
+           NextDayStartsAt: LocalTime
+           LearnAheadLimit: Duration
+           TimeboxTimeLimit: Duration
+           IsNightMode: bool
+           Created: Instant
+           Modified: Instant
+           Timezone: DateTimeZone
+        }
+
+    type Event =
+        | DefaultCardSettingIdChanged of {| CardSettingId: CardSettingId |}
+        | DefaultDeckIdChanged        of {| DeckId: DeckId |}
+        | Snapshotted                 of Snapshotted
+        interface UnionContract.IUnionContract
+    
+    let codec = Codec.Create<Event>()
+
+module Fold =
+    
+    type State =
+        | Initial
+        | Active of Events.Snapshotted
+    let initial = State.Initial
+
+    let evolve state =
+        function
+        | Events.Snapshotted s -> State.Active s
+        | Events.DefaultCardSettingIdChanged b ->
+            match state with
+            | State.Initial  -> invalidOp "User doesn't exist"
+            | State.Active a -> { a with DefaultCardSettingId = b.CardSettingId } |> State.Active
+        | Events.DefaultDeckIdChanged b ->
+            match state with
+            | State.Initial  -> invalidOp "User doesn't exist"
+            | State.Active a -> { a with DefaultDeckId = b.DeckId } |> State.Active
+    
+    let fold : State -> Events.Event seq -> State = Seq.fold evolve
+    let isOrigin = function Events.Snapshotted _ -> true | _ -> false
+
+let decideCreate state = function
+    | Fold.State.Initial  -> Ok ()                  , [ Events.Snapshotted state ]
+    | Fold.State.Active _ -> Error "Already created", []
+
+type Service internal (resolve) =
+
+    member _.Create(state: Events.Snapshotted) =
+        let stream : Stream<_, _> = resolve state.UserId
+        stream.Transact(decideCreate state)
+
+let create resolve =
+    let resolve id = Stream(Log.ForContext<Service>(), resolve (streamName id), maxAttempts=3)
+    Service(resolve)
