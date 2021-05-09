510ef0a84ffa01eacf4ac567e7037a2045b9dba3
Alex <dharmaturtle@gmail.com>
2021-05-09 13:14:07 -0500

    Dexie supports Stack events and summaries

--- CardOverflow.Server/Dexie.cs
+++ CardOverflow.Server/Dexie.cs
@@ -18,10 +18,12 @@ namespace CardOverflow.Server {
     private readonly IJSRuntime _jsRuntime;
 
     const string BULK_PUT_EVENTS = "bulkPutEvents";
-    const string GET_UNSYNCED_EVENTS = "getUnsyncedEvents";
+    const string GET_ALL_UNSYNCED = "getAllUnsynced";
 
     const string DECK_PREFIX = "Deck";
+    const string STACK_PREFIX = "Stack";
     const string DECK_STREAM = "DeckStream";
+    const string STACK_STREAM = "StackStream";
 
     public Dexie(IJSRuntime jsRuntime) => _jsRuntime = jsRuntime;
 
@@ -34,9 +36,23 @@ namespace CardOverflow.Server {
       await _jsRuntime.InvokeVoidAsync(BULK_PUT_EVENTS, DECK_PREFIX, eventsString, summariesString);
     }
 
-    public async Task<IList<ClientEvent<Deck.Events.Event>>> GetUnsyncedDeckEvents() {
-      var events = await _jsRuntime.InvokeAsync<JsonElement>(GET_UNSYNCED_EVENTS, DECK_STREAM);
-      return events.EnumerateArray().Select(e => Serdes.Deserialize<ClientEvent<Deck.Events.Event>>(e.GetString(), jsonSerializerSettings)).ToList();
+    public async Task Append(IEnumerable<ClientEvent<Stack.Events.Event>> events) {
+      var summaries = events.GroupBy(x => x.StreamId).ToDictionary(
+        e => e.Key,
+        e => Stack.Fold.fold.Invoke(Stack.Fold.initial).Invoke(e.Select(x => x.Event)));
+      var eventsString = Serdes.Serialize(events, jsonSerializerSettings);
+      var summariesString = Serdes.Serialize(summaries, jsonSerializerSettings);
+      await _jsRuntime.InvokeVoidAsync(BULK_PUT_EVENTS, STACK_PREFIX, eventsString, summariesString);
+    }
+
+    public async Task<(
+      IList<ClientEvent<Deck.Events.Event>>,
+      IList<ClientEvent<Stack.Events.Event>>
+    )> GetAllUnsynced() {
+      var events = (await _jsRuntime.InvokeAsync<JsonElement>(GET_ALL_UNSYNCED)).EnumerateArray().ToList();
+      var deckEvents = events[0].EnumerateArray().Select(e => Serdes.Deserialize<ClientEvent<Deck.Events.Event>>(e.GetString(), jsonSerializerSettings)).ToList();
+      var stackEvents = events[1].EnumerateArray().Select(e => Serdes.Deserialize<ClientEvent<Stack.Events.Event>>(e.GetString(), jsonSerializerSettings)).ToList();
+      return (deckEvents, stackEvents);
     }
 
   }
--- CardOverflow.Server/wwwroot/js/dexie.js
+++ CardOverflow.Server/wwwroot/js/dexie.js
@@ -3,22 +3,40 @@ const unsynced = "1970-01-01T00:00:13Z"; // see `unsynced` in Infrastructure.fs
 function getDb() {
     let db = new Dexie("MainDatabase");
     db.version(1).stores({
-        DeckStream: "commandId,streamId,clientCreatedAt,serverCreatedAt",
-        DeckSummary: "id,name,description",
+        DeckStream  : "commandId,streamId,clientCreatedAt,serverCreatedAt",
+        StackStream : "commandId,streamId,clientCreatedAt,serverCreatedAt",
+        DeckSummary : "id,name,description",
+        StackSummary: "id",
     });
     return db;
 }
 
-function bulkPutEvents(tablePrefix, eventsString, summaryString) {
-    let parsedSummaries = JSON.parse(summaryString);
-    let summaries = Object.keys(parsedSummaries).map(key => {
+function deckSummaries(summaries) {
+    return Object.keys(summaries).map(key => {
         return {
             id         : key,
-            name       : parsedSummaries[key].Fields[0].Name,
-            description: parsedSummaries[key].Fields[0].Description,
-            summary    : JSON.stringify(parsedSummaries[key])
+            name       : summaries[key].Fields[0].Name,
+            description: summaries[key].Fields[0].Description,
+            summary    : JSON.stringify(summaries[key])
         };
     });
+}
+
+function stackSummaries(summaries) {
+    return Object.keys(summaries).map(key => {
+        return {
+            id     : key,
+            summary: JSON.stringify(summaries[key])
+        };
+    });
+}
+
+function bulkPutEvents(tablePrefix, eventsString, summaryString) {
+    let parsedSummaries = JSON.parse(summaryString);
+    let summaries =
+        tablePrefix === "Deck"  ? deckSummaries(parsedSummaries)  :
+        tablePrefix === "Stack" ? stackSummaries(parsedSummaries) :
+        (function () { throw "Unsupported table: " + tablePrefix }());
     let events = JSON.parse(eventsString).map(event => {
         return {
             commandId      : event.Event.Fields[0].Meta.CommandId,
@@ -37,10 +55,19 @@ function bulkPutEvents(tablePrefix, eventsString, summaryString) {
     });
 }
 
-function getUnsyncedEvents(table) {
-    return getDb()
-        .table(table)
+function getUnsynced(table) {
+    return table
         .where('serverCreatedAt')
         .equals(unsynced)
         .toArray(row => { return row.map(e => e.event) }); // can't "select" just the event from indexeddb, so we `map` it https://github.com/dfahlander/Dexie.js/issues/468
+}
+
+function getAllUnsynced() {
+    let db = getDb();
+    return db.transaction('r', "DeckStream", "StackStream", async () => {
+        return Promise.all([
+            getUnsynced(db.table("DeckStream")),
+            getUnsynced(db.table("StackStream"))
+        ]);
+    });
 };
