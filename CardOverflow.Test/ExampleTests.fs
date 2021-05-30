e107e182afaee86be148f2ee62ce7d11b24e5a40
Alex <dharmaturtle@gmail.com>
2021-05-29 22:42:09 -0500

    Fixed a test

--- CardOverflow.Test/ExampleTests.fs
+++ CardOverflow.Test/ExampleTests.fs
@@ -17,7 +17,10 @@ open FsToolkit.ErrorHandling
 open Domain.Projection
 
 [<StandardProperty>]
-let ``ExampleAppender roundtrips`` { SignedUp = signedUp; TemplateCreated = templateCreated; TemplateEdited = templateEdited; ExampleCreated = exampleCreated; ExampleCreated2 = exampleCreated2; Edit = exampleEdited; StackCreated = stackCreated } = asyncResult {
+let ``ExampleAppender roundtrips`` { SignedUp = signedUp; TemplateCreated = templateCreated; TemplateEdited = templateEdited; ExampleCreated = exampleCreated; ExampleCreated2 = exampleCreated2; Edit = exampleEdited; StackCreated = stackCreated } (meta1: Meta) (meta2: Meta) (meta3: Meta) = asyncResult {
+    let meta1 = { meta1 with UserId = signedUp.Meta.UserId }
+    let meta2 = { meta2 with UserId = signedUp.Meta.UserId }
+    let meta3 = { meta3 with UserId = signedUp.Meta.UserId }
     let c = TestEsContainer()
     do! c.UserSagaAppender().Create signedUp
     do! c.TemplateAppender().Create templateCreated
@@ -50,39 +53,47 @@ let ``ExampleAppender roundtrips`` { SignedUp = signedUp; TemplateCreated = temp
     do! exampleAppender.Edit exampleEdited exampleCreated.Id
     
     let! actual = c.KeyValueStore().GetExample exampleCreated.Id
-    exampleSummary |> Example.Fold.evolveEdited exampleEdited |> Assert.equal (actual |> Kvs.toExample)
+    let exampleSummary = exampleSummary |> Example.Fold.evolveEdited exampleEdited
+    Assert.equal (actual |> Kvs.toExample) exampleSummary
 
     let! cs = collectors()
     Assert.equal [0;1] cs
 
     (***   when template edited, then azure table updated   ***)
     do! c.TemplateAppender().Edit templateEdited templateCreated.Id
-    let exampleEdited_T = { exampleEdited with TemplateRevisionId = templateCreated.Id, templateEdited.Ordinal; Ordinal = exampleEdited.Ordinal + 1<exampleRevisionOrdinal> }
+    let exampleEdited_T =
+        { exampleEdited with
+            Meta = meta3
+            TemplateRevisionId = templateCreated.Id, templateEdited.Ordinal
+            Ordinal = exampleEdited.Ordinal + 1<exampleRevisionOrdinal> }
     do! exampleAppender.Edit exampleEdited_T exampleCreated.Id
     
     let! actual = c.KeyValueStore().GetExample exampleCreated.Id
-    exampleSummary |> Example.Fold.evolveEdited exampleEdited |> Example.Fold.evolveEdited exampleEdited_T |> Assert.equal (actual |> Kvs.toExample)
+    let exampleSummary = exampleSummary |> Example.Fold.evolveEdited exampleEdited_T
+    Assert.equal (actual |> Kvs.toExample) exampleSummary
 
     let! cs = collectors()
     Assert.equal [0;0;1] cs
 
     (***   when Stack's Revision changed, then azure table updated   ***)
-    let revisionChanged : Stack.Events.RevisionChanged = { Meta = signedUp.Meta; RevisionId = exampleCreated.Id, exampleEdited_T.Ordinal }
+    let revisionChanged : Stack.Events.RevisionChanged = { Meta = meta1; RevisionId = exampleCreated.Id, exampleEdited_T.Ordinal }
     do! stackAppender.ChangeRevision revisionChanged stackCreated.Id
     
     let! actual = c.KeyValueStore().GetStack stackCreated.Id
-    stackSummary |> Stack.Fold.evolveRevisionChanged revisionChanged |> Assert.equal actual
+    let stackSummary = stackSummary |> Stack.Fold.evolveRevisionChanged revisionChanged
+    Assert.equal actual stackSummary
     
     let! cs = collectors()
     Assert.equal [1;0;0] cs
 
     (***   when Stack's Revision changed to new Example, then azure table updated   ***)
     do! exampleAppender.Create exampleCreated2
-    let revisionChanged : Stack.Events.RevisionChanged = { Meta = signedUp.Meta; RevisionId = exampleCreated2.Id, Example.Fold.initialExampleRevisionOrdinal }
+    let revisionChanged : Stack.Events.RevisionChanged = { Meta = meta2; RevisionId = exampleCreated2.Id, Example.Fold.initialExampleRevisionOrdinal }
     do! stackAppender.ChangeRevision revisionChanged stackCreated.Id
     
     let! actual = c.KeyValueStore().GetStack stackCreated.Id
-    stackSummary |> Stack.Fold.evolveRevisionChanged revisionChanged |> Assert.equal actual
+    let stackSummary = stackSummary |> Stack.Fold.evolveRevisionChanged revisionChanged
+    Assert.equal actual stackSummary
     
     let! cs = collectors()
     Assert.equal [0;0;0] cs
